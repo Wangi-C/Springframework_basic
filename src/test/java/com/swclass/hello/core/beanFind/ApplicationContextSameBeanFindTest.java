925f08ee397bdcc2ea1b021aed56b25cb115e633
Wangi-C <kiiiiwcho@gmail.com>
2022-11-21 23:19:49 +0900

    스프링 빈 조회 (컨테이너에 같은 타입의 빈이 두개 이상일 경우) 타입으로 조회시, NoUniqueBeanDefinitionException 발생  -> 이름으로 조회해 준다.

---
+++ src/test/java/com/swclass/hello/core/beanFind/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,48 @@
+package com.swclass.hello.core.beanFind;
+
+import com.swclass.hello.core.AppConfig;
+import com.swclass.hello.core.discount.DiscountPolicy;
+import com.swclass.hello.core.member.MemberRepository;
+import com.swclass.hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextSameBeanFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생")
+    void findBeanByTypeDuplicate() {
+//        MemberRepository bean = ac.getBean(MemberRepository.class);
+        assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(MemberRepository.class));
+    }
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름으로 지정하면 된다")
+    void findBeanByName() {
+        MemberRepository memberRepository1 = ac.getBean("memberRepository1", MemberRepository.class);
+        assertThat(memberRepository1).isInstanceOf(MemberRepository.class);
+    }
+
+    @Configuration
+    static class SameBeanConfig {
+        @Bean
+        public MemberRepository memberRepository1() {
+            return new MemoryMemberRepository();
+        }
+
+        @Bean
+        public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
+    }
+}
