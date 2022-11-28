31b4bd2744bb4bbf8ed3feb0258ce3729f52fa4c
WangiC <ChoWangi>
2022-11-28 20:52:23 +0900

    BeanDefinition (빈 설정정보) 스프링은 설정 정보가 .class 이든 .xml이든 관심이 없다. 오로지 BeanDefinition에만 집중을 한다. 각 설정정보에 맞는 Reader객체가 설정정보를 읽어서 BeanDefinition을 만든다.

---
+++ src/test/java/com/swclass/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,20 @@
+package com.swclass.hello.core.xml;
+
+import com.swclass.hello.core.member.Member;
+import com.swclass.hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext () {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+
+}
