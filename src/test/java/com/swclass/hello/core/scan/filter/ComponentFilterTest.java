7f728339c285e209231089c37c28d14f631b5825
Wangi-C <kiiiiwcho@gmail.com>
2022-12-06 23:23:33 +0900

    <필터> includeFilter = @ComponentScan.Filter(type=FilterType.ANNOTATION, classes=.class), excludeFilter = @ComponentScan.Filter(type=FilterType.ANNOTATION, classes=.class)
    스캔할 애노테이션과 스캔하지 않을 애노테이션을 직접 지정할 수 있다.

---
+++ src/test/java/com/swclass/hello/core/scan/filter/ComponentFilterTest.java
@@ -0,0 +1,36 @@
+package com.swclass.hello.core.scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ComponentFilterTest {
+
+    @Test
+    void fiterScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        assertThat(beanA).isNotNull();
+
+        assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class)
+        );
+    }
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+    )
+    static class TestConfig {
+
+    }
+}
