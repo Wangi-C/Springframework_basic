212c0192a1c1e5f537230ddc45e13c92d30e6ceb
Wangi-C <kiiiiwcho@gmail.com>
2022-12-06 22:22:35 +0900

    <컴포넌트 스캔과 자동 의존 주입> 기존 : @Configuration 설정파일에서 직접 Bean을 다 등록해주고 의존주입해주었다. -> 만약 Bean이 수백개, 수천개 등록되어야 한다면?
    컴포넌트 스캔 : @Configuraion & @ComponentScan 이 있는 설정파일은 @ComponentScan 대상(@Component, @Configuration 등)을 자동 Bean 등록해준다.
    -> 그럼 빈 간 의존성 주입은 어떻게 할 것인가?
       @Autowired 를 사용하여 자동 의존 주입을 해준다.

--- src/main/java/com/swclass/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/com/swclass/hello/core/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package com.swclass.hello.core.discount;
 
 import com.swclass.hello.core.member.Grade;
 import com.swclass.hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
