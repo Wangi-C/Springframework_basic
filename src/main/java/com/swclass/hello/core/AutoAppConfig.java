ec5bceca91b22e90ccf6277d8f8dfe60d3e382e6
Wangi-C <kiiiiwcho@gmail.com>
2022-12-06 23:05:29 +0900

    <scan 위치와 기본 스캔 대상> scan 위치는 @Component의 basePackages = "패키지경로"로 지정할 수 있다. 해당 경로 하위의 ComponentScan대상들을 bean으로 등록한다. -> basePackages가 설정되어 있지 않으면?     default) @ComponentScan이 있는 설정파일의 패키지 하위의 Bean을 등록한다. -> ComponentScan 대상?     @Component, @Controller, @Service, @Repository, @Configuration

--- src/main/java/com/swclass/hello/core/AutoAppConfig.java
+++ src/main/java/com/swclass/hello/core/AutoAppConfig.java
@@ -6,7 +6,16 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "com.swclass.hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
+//scan 범위를 지정하지 않으면 default로 현재 class의 Package(com.swclass.hello.core)하위의 class를 스캔
+//메인 실행 class - HeeloSpringApplication의 @SpringBootApplication이 기본적으로
+//ComponentScan 을 해준다.(최상단에서)
+/**
+ * 컴포넌트 스캔 기본 대상
+ * Component, Controller, Service, Repository, Configuration
+ * **/
 public class AutoAppConfig {
 }
