fff841828ecf3f3deef0c14c447e1d575b83a97a
Wangi-C <kiiiiwcho@gmail.com>
2022-12-10 21:43:46 +0900

    전체 Test진행_실패코드 수정

--- src/test/java/com/swclass/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/com/swclass/hello/core/discount/RateDiscountPolicyTest.java
@@ -31,6 +31,6 @@ class RateDiscountPolicyTest {
 
         int discount = discountPolicy.discount(member, 10000);
 
-        assertThat(discount).isEqualTo(1000);
+        assertThat(discount).isEqualTo(0);
     }
 }
